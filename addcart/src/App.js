5f7783c56746b1da6443f1d2db33a2dc9b23790b
Pushpendra Tripathi <trippushpendra195@gmail.com>
2022-09-16 22:21:59 +0530

    redux done

--- addcart/src/App.js
+++ addcart/src/App.js
@@ -1,18 +1,19 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import CardsDetails from './components/CardsDetails';
 import Cards from './components/Cards';
-import {Routes,Route} from 'react-router-dom';
+import {Routes,Route} from "react-router-dom";
 
 function App() {
   return (
-    <>
-    <Header/>
-    <Routes>
-    <Route path='/' element={<Cards />} />
-     <Route path='/cart' element={<CardsDetails />} />
-    </Routes>
-    </>
+  <>
+   <Header />
+   <Routes>
+     <Route path='/' element={<Cards />} />
+     <Route path='/cart/:id' element={<CardsDetails />} />
+   </Routes>
+  </>
   );
 }
 
